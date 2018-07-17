58f2f904a86ecf406186f646dbe419f601ef25a6
kd241286zjv <kd241286zjv@gmail.com>
2018-07-17 16:02:11 +0300

    update

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
       prevEl: '.swiper-button-prev',
     },
   });
+  $('.scrolldown').click(function(){
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
 });
